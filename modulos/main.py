0285c438b3220fcf62661a4486026f5effbef715
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-03-09 18:42:45 -0300

    first commit

---
+++ modulos/main.py
@@ -0,0 +1,11 @@
+import math_operations
+import string_utils
+
+print(math_operations.sum(5, 7))
+print(math_operations.subtract(5, 7))
+print(math_operations.multiply(5, 7))
+print(math_operations.divide(5, 7))
+
+print(string_utils.capitalize("hello"))
+print(string_utils.reverse_strings("hello"))
+print(string_utils.count("hello"))
---
+++ modulos/string_utils.py
@@ -0,0 +1,10 @@
+# Modulos de operações com strings
+
+def capitalize(s):
+  return s.capitalize()
+
+def reverse_strings(s):
+  return s[::-1]
+
+def count(s):
+  return len(s)
