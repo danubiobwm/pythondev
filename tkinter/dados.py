bb9c79a745ac88fa117f36b235def9d84af699ca
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-05-16 16:41:59 -0300

    projeto tkinter pokemon

---
+++ tkinter/dados.py
@@ -0,0 +1,66 @@
+pokemon = {
+    "Pikachu": {
+        "nome": "Pikachu",
+        "numero": 25,
+        "tipo": ["Elétrico"],
+        "habilidades": ["Estático", "Absorção de Luz"],
+        "imagem": "img/pikachu.png",
+        "outros": ["#fdd835"],
+        "status": [35, 55, 40, 90]
+    },
+    "Bulbasaur": {
+        "nome": "Bulbasaur",
+        "numero": 1,
+        "tipo": ["Planta", "Venenoso"],
+        "habilidades": ["Esporo", "Clorofila"],
+        "imagem": "img/bulbasaur.png",
+        "outros": ["#4caf50"],
+        "status": [45, 49, 49, 45]
+    },
+    "Charmander": {
+        "nome": "Charmander",
+        "numero": 4,
+        "tipo": ["Fogo"],
+        "habilidades": ["Chama", "Corpo Chama"],
+        "imagem": "img/charmander.png",
+        "outros": ["#ff9800"],
+        "status": [39, 52, 43, 65]
+    },
+    "Squirtle": {
+        "nome": "Squirtle",
+        "numero": 7,
+        "tipo": ["Água"],
+        "habilidades": ["Torrent", "Forte Chuva"],
+        "imagem": "img/squirtle.png",
+        "outros": ["#2196f3"],
+        "status": [44, 48, 65, 43]
+    },
+    "Gyarados": {
+        "nome": "Gyarados",
+        "numero": 130,
+        "tipo": ["Água", "Voador"],
+        "habilidades": ["Intimidate", "Moxie"],
+        "imagem": "img/gyarados.png",
+        "outros": ["#1976d2"],
+        "status": [95, 125, 79, 81]
+    },
+    "Gengar": {
+        "nome": "Gengar",
+        "numero": 94,
+        "tipo": ["Fantasma", "Venenoso"],
+        "habilidades": ["Corpo Amaldiçoado"],
+        "imagem": "img/gengar.png",
+        "outros": ["#9c27b0"],
+        "status": [60, 65, 60, 110]
+    },
+    "Dragonite": {
+        "nome": "Dragonite",
+        "numero": 149,
+        "tipo": ["Dragão", "Voador"],
+        "habilidades": ["Inner Focus", "Multiscale"],
+        "imagem": "img/dragonite.png",
+        "outros": ["#00bcd4"],
+        "status": [91, 134, 95, 80]
+    },
+    # Adicione os outros Pokémon que você quer aqui, seguindo a mesma estrutura
+}
