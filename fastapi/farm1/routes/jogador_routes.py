2c4076002daf5cd9047ed340a885913964e018e6
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-05-19 18:27:47 -0300

    projeto fastapi jogador

--- fastapi/farm1/routes/jogador_routes.py
+++ fastapi/farm1/routes/jogador_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from config.database import db
 from models.jogador_models import JogadorModels
 from schemas.jogador_schema import jogadorEntity, ListaJogadoresEntity
+from bson import ObjectId
 
 jogador_router = APIRouter()
 
@@ -21,3 +22,22 @@ async def cadastra_jogador(jogador: JogadorModels):
     db.db_play.jogador.insert_one(dict(jogador))
     return jogadorEntity(jogador)
 
+#detalhes jogador
+@jogador_router.get("/jogadores/{jogador_id}")
+async def detalhes_jogador(jogador_id):
+    jogador = db.db_play.jogador.find_one({"_id": ObjectId(jogador_id)})
+    if jogador:
+        return jogadorEntity(jogador)
+    return {"error": "Jogador não encontrado"}
+
+#Atualiza jogador
+@jogador_router.put("/jogadores/{jogador_id}")
+async def atualiza_jogador(jogador_id, jogador: JogadorModels):
+    db.db_play.jogador.update_one({"_id": ObjectId(jogador_id)}, {"$set": dict(jogador)})
+    return jogadorEntity(jogador)
+
+#Deleta jogador
+@jogador_router.delete("/jogadores/{jogador_id}")
+async def deleta_jogador(jogador_id):
+    db.db_play.jogador.delete_one({"_id": ObjectId(jogador_id)})
+    return {"message": "Jogador deletado com sucesso"}
--- fastapi/farm1/schemas/jogador_schema.py
+++ fastapi/farm1/schemas/jogador_schema.py
@@ -1,12 +1,20 @@
 
 
 def jogadorEntity(db_item)-> dict:
-    return {
-        "id": str(db_item["_id"]),
-        "nome": db_item["jogador_nome"],
-        "idade": db_item["jogador_idade"],
-        "time": db_item["jogador_time"]
-    }
+     if isinstance(db_item, dict):
+        return {
+            "id": str(db_item["_id"]),
+            "nome": db_item["jogador_nome"],
+            "idade": db_item["jogador_idade"],
+            "time": db_item["jogador_time"]
+        }
+     else:
+       return {
+            "id": str(getattr(db_item, "_id", None)),
+            "nome": db_item.jogador_nome,
+            "idade": db_item.jogador_idade,
+            "time": db_item.jogador_time
+        }
 
 def ListaJogadoresEntity(db_item_lista)-> list:
     lista_jogadores = []
