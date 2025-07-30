9aa570b01f758ed2de777ea88dfa25d431c9c675
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-07-30 19:02:56 -0300

    todo service

--- fastapi/fast_todo/app/api/api_v1/handlers/todo.py
+++ fastapi/fast_todo/app/api/api_v1/handlers/todo.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter
-from schemas.todo_schema import TodoCreate, TodoUpdate, TodoDetail
+from fastapi import APIRouter, Depends
+from schemas.todo_schema import TodoCreate, TodoDetail
 from models.user_model import User
 from api.dependencies.user_deps import get_current_user
+from services.todo_service import TodoService
+from models.todo_model import Todo
+from typing import List
 
 
 todo_router = APIRouter()
 
 @todo_router.get("/", summary="List all todos", response_model=TodoDetail)
 async def List(current_user: User = Depends(get_current_user)):
-    pass  # Implementation for listing todos
+    """
+    List all todos for the current user.
+    """
+    return await TodoService.list_todos(current_user)
+
+@todo_router.post("/create", summary="Create a new todo", response_model=TodoDetail)
+async def Create(data: TodoCreate, current_user: User = Depends(get_current_user)):
+    """
+    Create a new todo for the current user.
+    """
+    return await TodoService.create_todo(current_user, data)
--- fastapi/fast_todo/app/services/todo_service.py
+++ fastapi/fast_todo/app/services/todo_service.py
@@ -1,2 +1,16 @@
 from models.user_model import User
 from models.todo_model import Todo
+from typing import List
+from schemas.todo_schema import TodoCreate, TodoUpdate
+
+class TodoService:
+  @staticmethod
+  async def list_todos(user: User) -> List[Todo]:
+    todos = await Todo.find(Todo.owner.id== user.id).to_list()
+    return todos
+
+  @staticmethod
+  async def create_todo(user: User, data: TodoCreate) -> Todo:
+      todo = Todo(**data.dict(), owner=user)
+      return await todo.insert()
+
