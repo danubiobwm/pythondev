972c0f369371b02baa02a7c7e9dadbf53026eb83
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-08-05 19:44:36 -0300

    cors

--- fastapi/fast_todo/app/app.py
+++ fastapi/fast_todo/app/app.py
@@ -6,9 +6,22 @@ from models.user_model import User
 from models.todo_model import Todo
 from api.api_v1.router import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=600,
 )
 
 @app.on_event("startup")
