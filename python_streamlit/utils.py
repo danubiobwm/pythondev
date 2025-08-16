7c9548d2d65265556d406db8b50e1379b1552dd1
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-08-15 21:25:21 -0300

    dataset

--- python_streamlit/utils.py
+++ python_streamlit/utils.py
@@ -1,6 +1,14 @@
+from dataset import df
+
 def format_number(value, prefix=''):
     for unit in["", "mil"]:
         if abs(value) < 1000:
             return f"{prefix}{value:.2f}{unit}"
         value /= 1000
     return f"{prefix}{value:.2f}Millhões"
+
+
+df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
+df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on='Local da compra', right_index=True, how='left').sort_values(by='Preço', ascending=False)
+
+print(df_rec_estado.head())
