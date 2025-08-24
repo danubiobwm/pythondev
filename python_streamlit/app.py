7c8563172fdbe8fac3bf0f5c8d9daca5e153a940
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-08-23 21:50:24 -0300

    grafico

--- python_streamlit/app.py
+++ python_streamlit/app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import plotly.express as px
 from dataset import df
 from utils import format_number
+from graficos import grafico_map_estado
 
 # Sample data for demonstration
 st.set_page_config(page_icon=":bar_chart:", layout="wide")
@@ -17,6 +18,7 @@ with aba2:
   coluna1, coluna2 = st.columns(2)
   with coluna1:
     st.metric("Receita Total", format_number(df['Preço'].sum(), 'R$'))
+    st.plotly_chart(grafico_map_estado, use_container_width=True)
   with coluna2:
     st.metric("Quantidade Vendas", df.shape[0])
 
