ea250e4d0f294b0638cd54c18e1e1a382a172b8a
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-09-22 22:44:04 -0300

    dataframe receita mensal

--- python_streamlit/utils.py
+++ python_streamlit/utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dataset import df
 
 def format_number(value, prefix=''):
@@ -7,8 +8,15 @@ def format_number(value, prefix=''):
         value /= 1000
     return f"{prefix}{value:.2f}Millhões"
 
-
+# 1- Dataframe Receita por Estado
 df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
-df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on='Local da compra', right_index=True, how='left').sort_values(by='Preço', ascending=False)
+df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
 
 print(df_rec_estado.head())
+
+# 2 - Dataframe Receita Mensal
+df_rec_mensal = df.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].sum().reset_index()
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name()
+print(df_rec_mensal)
