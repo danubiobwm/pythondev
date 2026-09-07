41b52ebd7fb3e1240c7b05e4c6dcf9c70b842d27
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-09-06 21:38:15 -0300

    model, config admin

--- ApiEventosDjango/eventin/admin.py
+++ ApiEventosDjango/eventin/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Evento, Participante
 
-# Register your models here.
+
+class Eventos(admin.ModelAdmin):
+  list_display = ('id', 'titulo', 'local', 'data_evento', 'capacidade', 'horario')
+  list_display_links = ('id', 'titulo')
+  search_fields = ('titulo', 'local')
+  list_per_page = 10
+
+
+class Participantes(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'cpf', 'email', 'telefone')
+  list_display_links = ('id', 'nome')
+  search_fields = ('nome', 'email')
+  list_per_page = 10
+
+
+
+admin.site.register(Evento, Eventos)
+admin.site.register(Participante, Participantes)
--- ApiEventosDjango/eventin/models.py
+++ ApiEventosDjango/eventin/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+  titulo = models.CharField(max_length=100)
+  descricao = models.TextField()
+  local = models.CharField(max_length=100)
+  data_evento = models.DateField()
+  horario = models.TimeField()
+  capacidade = models.PositiveIntegerField()
+
+  def __str__(self):
+    return self.titulo
+
+class Participante(models.Model):
+  nome = models.CharField(max_length=100)
+  cpf = models.CharField(max_length=11)
+  email = models.EmailField(unique=True)
+  telefone = models.CharField(max_length=15)
+
+  def __str__(self):
+    return self.nome
+
