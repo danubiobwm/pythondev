59323488338cd57c8641431f9f8a2e71da99b0be
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-09-08 13:06:21 -0300

    models de inscritos

--- ApiEventosDjango/eventin/models.py
+++ ApiEventosDjango/eventin/models.py
@@ -22,3 +22,10 @@ class Participante(models.Model):
   def __str__(self):
     return self.nome
 
+class Inscricao(models.Model):
+  evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='inscricoes')
+  participante = models.ForeignKey(Participante, on_delete=models.CASCADE, related_name='inscricoes')
+  data_inscricao = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.participante.nome} - inscrito no evento: {self.evento.titulo}"
