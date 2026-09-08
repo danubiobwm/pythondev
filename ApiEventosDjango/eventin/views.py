bc7893a2478669106a64340a46f931e1d51d6238
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-09-08 16:03:33 -0300

    Auth

--- ApiEventosDjango/eventin/views.py
+++ ApiEventosDjango/eventin/views.py
@@ -1,16 +1,24 @@
 from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Evento, Participante, Inscricao
 from .serializers import EventoSerializer, ParticipanteSerializer, InscricaoSerializer, ListInscricaoParticipanteSerializer, ListInscricaoEventoSerializer
 
 class EventosViewSet(viewsets.ModelViewSet):
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
   queryset = Evento.objects.all()
   serializer_class = EventoSerializer
 
 class ParticipantesViewSet(viewsets.ModelViewSet):
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
   queryset = Participante.objects.all()
   serializer_class = ParticipanteSerializer
 
 class InscricoesViewSet(viewsets.ModelViewSet):
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
   queryset = Inscricao.objects.all()
   serializer_class = InscricaoSerializer
 
